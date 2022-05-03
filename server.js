73f5d06c79cd29951dbabc3fdbcc7375dc8e1e7c
saurabhpojhi <saurabh.kumarjan10@gmail.com>
2022-05-03 16:29:51 +0530

    server

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+// set port, listen for requests
+const PORT = process.env.PORT || 3000;
+
+
+var corsOptions = {
+    origin: "http://localhost:3000"
+  };
+
+  app.use(cors(corsOptions));
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+// simple route
+
+app.get("/", (req, res) => {
+    res.json({ message: "Rest API." });
+  });
+
+  
