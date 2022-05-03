92ade107cda39147397d5bd239ad9d3b65712eb4
saurabhpojhi <saurabh.kumarjan10@gmail.com>
2022-05-03 22:51:46 +0530

    add

--- models/index.js
+++ models/index.js
@@ -1,3 +1,10 @@
 const mongoose = require("mongoose");
-const dbConfig  = require("../config/db");
+const dbConfig  = require("../config/db.config");
 
+mongoose.Promise = global.Promise;
+const db = {};
+db.mongoose = mongoose;
+db.url = dbConfig.url;
+db.tutorials = require("./tutorial.model.js")(mongoose);
+
+module.exports = db;
